a390d90b045eabaf3678fe83f84001a2fd380668
Manuel Garcia <mgarcia@book.com>
2013-06-04 19:56:25 -0700

    Problem 12 solution.

--- problem12euler.c
+++ problem12euler.c
@@ -17,45 +17,31 @@ int getNextLowestPrime(int seed){
   int i = 0 ;
   for(i = seed+1 ; ; i++){
     if( isPrime(i) ){
-      //printf("returning %d \n",i);
       return i;
     }
   }
 }
 
-int countdivisors(unsigned long int num)
-{
-  int cnt =1;
-  unsigned long int i;
-  
-  for( i=2 ; i <= num ; i++ ){
-    if( (num % i ) == 0 ) {
-      cnt+=1;
-    }
-  }
-
-  return cnt;
-}
-
-int countdivisors2(unsigned long int num){
+int countdivisors(unsigned long int num){
   int countPrimes = 0;
   int prime=2;
   int seed =prime;
   int countDiv =1;
   while(num > 1){
-    if(! (num % prime) ){
+    while(! (num % prime) ){
       num /= prime ;
-      printf("num = %d \n",num);
       countPrimes++;
-    }else{
-      printf("countPrimes =%d \n",countPrimes);
-      countDiv *= (countPrimes+1);
-      countPrimes=1;
-      prime = getNextLowestPrime(seed) ;
-      seed=prime;
     }
+
+    if(countPrimes > 0 ){
+      countDiv *= (countPrimes+1);
+    }
+    countPrimes=0;
+    prime = getNextLowestPrime(seed) ;
+    seed=prime;
   }
-  return countDiv;
+  
+  return countDiv ;
 
 }
 
@@ -70,16 +56,13 @@ int main(){
   int i;
 
   unsigned long int triangular =0;
-  /*for(i=0; ; i++){
+  for(i=0; ; i++){
     triangular=getTriangular(i);
-    if( countdivisors2(triangular) >= 5){
+    if( countdivisors(triangular) >= 500){
       printf("Magic number is %lu for iteration %u \n",triangular,i);
       break;
     }
-    }*/
-
-  printf("countdivisors of 28 = %d ",countdivisors2(28));
-  //printf("Is prime(7) = %d ",isPrime(7));
+  }
 
   return 0;
 }
