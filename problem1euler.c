7b932dacbed496656f1ea67c96555c4b9ca76728
Manuel Garcia <alexgarcia2k@gmail.com>
2015-05-23 12:59:38 -0700

    Optimize problem 1 and put it in hackerrank format.

--- problem1euler.c
+++ problem1euler.c
@@ -1,18 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+unsigned long long getSumOfMultiples(long limit){
+    unsigned long long mul3=3;
+    unsigned long long mul5=5;
+    unsigned long long mul15=15;
+    unsigned long long total_sum =0;
+    unsigned long long sum_15 =0;
+    while(1){
+        if(limit < 3)
+            return 0;
+        if( mul3 < limit ){
+            total_sum += mul3;
+            mul3+=3;
+
+        }else{
+            break;
+        }
+        if( mul5 < limit){
+            total_sum += mul5;
+            mul5+=5;
+
+        }
+        if(limit  > 0 && 
+           mul15 < limit){
+            sum_15+=mul15;
+            mul15+=15;
+
+        }
+
+    }
+    
+    total_sum = total_sum - sum_15;
+    return total_sum;
+}
 
 int main()
 {
-  int total_sum = 0;
+  unsigned long long totalSum = 0;
+  long limit =1000000000;
+  int t=1,i;
 
-  int i = 0;
+  scanf("%d",&t);
 
-  for( i = 0 ; i < 1000 ; i++){
-    if( ( i % 3 == 0 ) || (i % 5 == 0) )
-      total_sum+=i;
+  for(i = 0; i < t; i++ ){
+      scanf("%ld",&limit);
+      totalSum = getSumOfMultiples(limit);
+      printf("%llu\n",totalSum);
   }
-
-  printf(" Total sum is equal to %d ", total_sum);
 
   return 0;
 }
